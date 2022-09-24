7a6aaaba5343657e3b06eeffdbec4487e2b8f94c
Oliver Brewka <olekatpyle@gmail.com>
2022-09-24 20:43:35 +0200

    update latest

---
+++ README.md
@@ -0,0 +1,9 @@
+## ONI theme for Qtile
+
+![ONI](https://raw.githubusercontent.com/olekatpyle/ONI_Qtile/main/.github/ONI.png)
+
+#### Features a foobar, a custom session manager.
+![ONI](https://raw.githubusercontent.com/olekatpyle/ONI_Qtile/main/.github/ONI_foobar.png)
+
+#### NOTE
+* no assets included
---
+++ config.py
@@ -0,0 +1,12 @@
+from modules.ONI import *
+
+dgroups_key_binder = None
+dgroups_app_rules = []
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+wmname = "Qtile"
+font = "Fira Code Regular Nerd Font Complete Mono"
+widget_defaults = dict(font=font, foreground="#ffffff", padding=3)
